5a6002590bf0436b682d8e27aab4b4b6729ab7af
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-12-14 19:48:03 +1100

    Refactored in a successHandler and an errorHandler

--- www/js/backbone.crypton.js
+++ www/js/backbone.crypton.js
@@ -22,6 +22,13 @@
   }
 
   Backbone.sync = function(method, model, options) {
+    var errorHandler = function (err, options) {
+      debug("ERROR: " + err);
+      return options.error && options.error(err);
+    };
+    var successHandler = function(resp) {
+      return options.success && options.success(resp);
+    };
     var _this = this;
     options = options || {};
     if (!window.app.session) {
@@ -38,39 +45,27 @@
     switch (method) {
       case "read":
         if (model.isNew && model.isNew()) {
-          return options.success && options.success(model.toJSON());
+          return successHandler(model.toJSON());
         }
         if (!model.isNew) {
           debug("COLLECTION, WHAT NOW?!");
           return;
         }
         window.app.session.load(container, function(err, entries) {
-          if (err) {
-            debug("ERROR: " + err);
-            return options.error && options.error(err);
-          }
+          if (err) return errorHandler(err, options);
           entries.get(model[model.idAttribute], function(err, entry) {
-            if (options.success) options.success(entry);
+            return successHandler(entry);
           });
         });
         break;
       case "create":
         window.app.session.load(container, function(err, entries) {
-          if (err) {
-            debug("ERROR: " + err);
-            return options.error && options.error(err);
-          }
+          if (err) return errorHandler(err, options);
           var modelId = guid();
           entries.add(modelId, function(err) {
-            if (err) {
-              debug("ERROR: " + err);
-              return options.error && options.error(err);
-            }
+            if (err) return errorHandler(err, options);
             entries.get(modelId, function(err, entry) {
-              if (err) {
-                debug("ERROR: " + err);
-                return options.error && options.error(err);
-              }
+              if (err) return errorHandler(err, options);
               var modelData = model.toJSON();
               modelData[model.idAttribute] = modelData[model.idAttribute] || modelId;
               for(var data in modelData) {
@@ -80,12 +75,9 @@
               }
               
               entries.save(function(err) {
-                if (err) {
-                  debug("ERROR: " + err);
-                  return options.error && options.error(err);
-                }
+                if (err) return errorHandler(err, options);
                 model[model.idAttribute] = modelId;
-                if (options.success) options.success(entry);
+                return successHandler(entry);
               });
             });
           });
@@ -93,18 +85,14 @@
         break;
       case "update":
         window.app.session.load(container, function(err, entries) {
-          if (err) {
-            debug("ERROR: " + err);
-            return options.error && options.error(err);
-          }
+          if (err) return errorHandler(err, options);
           entries.get(model[model.idAttribute], function(err, entry) {
             if (err) {
               if (err === "Key does not exist") {
                 Backbone.sync("create", model, options);
                 return;
               }
-              debug("ERROR: " + err);
-              return options.error && options.error(err);
+              return errorHandler(err, options);
             }
 
             for (var attribute in model.attributes) {
@@ -114,31 +102,22 @@
               }
             }
             entries.save(function(err) {
-              if (err) {
-                debug("ERROR: " + err);
-                return options.error && options.error(err);
-              }
-              return options.success && options.success(entry);
+              if (err) return errorHandler(err, options);
+              return successHandler(entry);
             });
           });
         });
         break;
       case "delete":
         window.app.session.load(container, function(err, entries) {
-          if (err) {
-            debug("ERROR: " + err);
-            return options.error && options.error(err);
-          }
+          if (err) return errorHandler(err, options);
           delete entries.keys[model[model.idAttribute]];
           if (model.isNew()) {
-            return options.success && options.success(false);
+            return successHandler(false);
           }
           entries.save(function(err) {
-            if (err) {
-              debug("ERROR: " + err);
-              return options.error && options.error(err);
-            }
-            return options.success && options.success(true);
+            if (err) return errorHandler(err, options);
+            return successHandler(true);
           });
         });
         break;
