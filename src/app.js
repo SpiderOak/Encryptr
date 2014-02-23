17b2820bf6b71770c1d9263f107cbe6d1c781f5d
Tommy-Carlos Williams <tommy@devgeeks.org>
2014-02-23 18:53:33 +1100

    Fix up Offline.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,34 @@ var Encryptr = (function (window, console, undefined) {
     window.crypton.host = "localhost";
 
     window.Offline.options =
-          {checks: {image: {url: "https://crypton.io/images/crypton.png"}}};
+      {
+        // Should we check the connection status immediatly on page load.
+        checkOnLoad: false,
+
+        // Should we monitor AJAX requests to help decide if we have a connection.
+        interceptRequests: true,
+
+        // Should we automatically retest periodically when the connection is down (set to false to disable).
+        reconnect: {
+          // How many seconds should we wait before rechecking.
+          initialDelay: 3
+        },
+
+        // Should we store and attempt to remake requests which fail while the connection is down.
+        requests: true,
+
+        // Should we show a snake game while the connection is down to keep the user entertained?
+        // It's not included in the normal build, you should bring in js/snake.js in addition to
+        // offline.min.js.
+        game: false,
+
+        // What the xhr checks
+        checks: {
+          xhr: {
+            url: "https://" + window.crypton.host + "/"
+          }
+        }
+      };
 
     var isNodeWebkit = (typeof process == "object");
     if (isNodeWebkit) $.os.nodeWebkit = true;
