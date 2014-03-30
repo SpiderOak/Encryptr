47a881ba8bcbd3093177d95af7984ba37323e292
Tommy-Carlos Williams <tommy@devgeeks.org>
2014-03-30 16:14:30 +1100

    Fix for the "toast" blocking part of the view even when hidden

--- src/views/ToastView.js
+++ src/views/ToastView.js
@@ -20,13 +20,18 @@
     show: function(message) {
       var _this = this;
       this.$el.html(message || "done");
+      this.$el.css({"z-index": "10002"});
       this.$el.css({"opacity": 0.7});
       window.setTimeout(function() {
         _this.hide();
       }, 1000);
     },
     hide: function() {
+      var _this = this;
       this.$el.css({"opacity": 0});
+      window.setTimeout(function(){
+        _this.$el.css({"z-index": "-1"});
+      }, 510);
     },
     close: function() {
       this.remove();
