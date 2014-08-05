b66b1367d04bbd97a0d8668ed7a0a06659702353
Ken Manheimer <ken.manheimer@gmail.com>
2014-08-04 20:13:16 -0400

    Consistent with crypton server localhost server defaulting to 1025.

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ var Encryptr = (function (window, console, undefined) {
     // Set the hostname for the Crypton server
     // window.crypton.host = "192.168.1.12";
     window.crypton.host = "localhost";
+    window.crypton.port = 1025;
 
     window.Offline.options = {
       // Should we check the connection status immediatly on page load.
