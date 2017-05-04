e36d04daf4dbd8c5edcda8584c4bd06274d0b399
Federico Gonzalez <federicogonzalez761@gmail.com>
2017-05-04 19:20:08 -0300

    Added tests for addDataFromEntry, addFieldFromEntry and generateCsvFromEntries

--- tests/export_csv.js
+++ tests/export_csv.js
@@ -28,19 +28,19 @@ describe('Export to Csv', function() {
       
       it('should have one field in fields', function() {
         view.addFieldFromEntry(entry, fields);
-        fields.should.not.be.eql({'0': 'item_name_1'});
+        fields.should.be.eql(['item_name_1']);
       });
       
       it('should have two field items in fields', function() {
         entry.items.push({key: 'item_name_2'});
         view.addFieldFromEntry(entry, fields);
-        fields.should.not.be.eql({'0': 'item_name_1', '1': 'item_name_2'});
+        fields.should.be.eql(['item_name_1', 'item_name_2']);
       });
 
       it('should not add repeat field', function() {
-          entry.items.push({key: 'item_name_1'});
+        entry.items.push({key: 'item_name_1'});
         view.addFieldFromEntry(entry, fields);
-        fields.should.not.be.eql({'0': 'item_name_1'});
+        fields.should.be.eql(['item_name_1']);
       });
 
     });
@@ -55,8 +55,8 @@ describe('Export to Csv', function() {
         fields = ['Entry Type', 'Label'];
       });
 
-      it('should have addFieldFromEntry method', function() {
-        view.addFieldFromEntry.should.be.an('function');
+      it('should have getCsvFields method', function() {
+        view.getCsvFields.should.be.an('function');
       });
 
       it('should call addFieldFromEntry', function() {
@@ -72,58 +72,92 @@ describe('Export to Csv', function() {
       });
 
       it('should have default fields in return', function() {
-        view.addFieldFromEntry(entry, fields);
-        fields.should.not.be.eql({'0': 'Entry Type', '1': 'Label'});
+        view.addFieldFromEntry({items: []}, fields);
+        fields.should.be.eql(['Entry Type', 'Label']);
       });
 
       it('should have default fields and entry field in return', function() {
         view.addFieldFromEntry(entry, fields);
-        fields.should.not.be.eql({'0': 'Entry Type', '1': 'Label', '2': 'item_name_1'});
+        fields.should.be.eql(['Entry Type', 'Label', 'item_name_1']);
       });
 
       it('should not add repeat field', function() {
         entry = {items: [{key: 'Entry Type'}]};
         entries = [entry];
         view.addFieldFromEntry(entry, fields);
-        fields.should.not.be.eql({'0': 'Entry Type', '1': 'Label'});
+        fields.should.be.eql(['Entry Type', 'Label']);
       });
 
     });
 
     describe('addDataFromEntry', function() {
-      it('should have addFieldFromEntry method', function() {
-        view.addFieldFromEntry.should.be.an('function');
+      
+     var entry, fields;
+
+      beforeEach(function() {
+        entry = {
+          type: 'entry_type',
+          label: 'entry_label',
+          items: [
+            {key: 'item_name_1', value: 'value_item_1'}
+          ]
+        };
+        fields = ['Entry Type', 'Label'];
+      });
+      
+      it('should have addDataFromEntry method', function() {
+        view.addDataFromEntry.should.be.an('function');
+      });
+
+      it('should return correct fields in entry', function() {
+        var entry_return = view.addDataFromEntry(entry, fields);
+        entry_return.should.have.keys('Entry Type', 'Label', 'item_name_1');
+      });
+
+      it('should return correct Entry Type in entry', function() {
+        var entry_return = view.addDataFromEntry(entry, fields);
+        entry_return['Entry Type'].should.be.eql('entry_type');
+      });
+
+      it('should return correct Label in entry', function() {
+        var entry_return = view.addDataFromEntry(entry, fields);
+        entry_return['Label'].should.be.eql('entry_label');
+      });
+
+      it('should return correct item fields in entry', function() {
+        var entry_return = view.addDataFromEntry(entry, fields);
+        entry_return['item_name_1'].should.be.eql('value_item_1');
       });
 
     });
 
     describe('getCsvData', function() {
-      it('should have addFieldFromEntry method', function() {
-        view.addFieldFromEntry.should.be.an('function');
+      it('should have getCsvData method', function() {
+        view.getCsvData.should.be.an('function');
       });
     });
 
     describe('writeCordovaFile', function() {
-      it('should have addFieldFromEntry method', function() {
-        view.addFieldFromEntry.should.be.an('function');
+      it('should have writeCordovaFile method', function() {
+        view.writeCordovaFile.should.be.an('function');
       });
     });
 
     describe('saveCsv', function() {
-      it('should have addFieldFromEntry method', function() {
-        view.addFieldFromEntry.should.be.an('function');
+      it('should have saveCsv method', function() {
+        view.saveCsv.should.be.an('function');
       });
     });
 
     describe('generateCsvFromEntries', function() {
-      it('should have addFieldFromEntry method', function() {
-        view.addFieldFromEntry.should.be.an('function');
+      it('should have generateCsvFromEntries method', function() {
+        view.generateCsvFromEntries.should.be.an('function');
       });
     });
 
     describe('getEntries', function() {
-      it('should have addFieldFromEntry method', function() {
-        view.addFieldFromEntry.should.be.an('function');
+      it('should have getEntries method', function() {
+        view.getEntries.should.be.an('function');
       });
     });
 
