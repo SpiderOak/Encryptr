2f92d38b3574871f609d211b07bc21e72a570ccc
Diego Marcovecchio <diego@spideroak-inc.com>
2017-08-09 15:55:17 -0300

    Add versioning to win installers.

--- resources/win-build-helper.py
+++ resources/win-build-helper.py
@@ -1,11 +1,22 @@
 import subprocess
 import shutil
 import os
+import sys
+
+'''
+This helper script will create the Encryptr MSI installer for you!
+call it using:
+$ python win-build-helper.py <VERSION>
+It requires the WiX Toolset to be installed in your machine!
+'''
+
+version_str = sys.argv[1]
 
 subprocess.call(["candle.exe",
-                "-ext", "WiXUtilExtension",
-                "-ext", "WixUIExtension",
-                "Encryptr-win.wxs"])
+                 "-ext", "WiXUtilExtension",
+                 "-ext", "WixUIExtension",
+                 "-dversion", version_str,
+                 "Encryptr-win.wxs"])
 
 inst_files_root = os.path.join(
     os.path.pardir, "desktopbuilds", "Encryptr", "win32")
@@ -16,9 +27,9 @@ shutil.copy2("GPLv3.rtf", inst_files_root)
 dist_str = os.path.abspath(inst_files_root)
 rtf_str = os.path.abspath(os.path.join(inst_files_root, "GPLv3.rtf"))
 
-call_str = 'light.exe -ext WiXUtilExtension -ext WixUIExtension -dWixUILicenseRtf="{}" -ddist="{}" -out Encryptr.msi Encryptr-win.wixobj'.format(rtf_str, dist_str)
-
-print("*************************")
-print(call_str)
+# I need do call this using a string and shell=True because the preferred
+# method will screw up the dirs by trying to escape everything.
+call_str = 'light.exe -ext WiXUtilExtension -ext WixUIExtension -dWixUILicenseRtf="{}" -ddist="{}" -dversion="{}" -out Encryptr.msi Encryptr-win.wixobj'.format(
+    rtf_str, dist_str, version_str)
 
 subprocess.call(call_str, shell=True)
